cfd0a72f23870b711db79f3ccbc7ee667e5dc99c
Jacqueline Askins <askinsj@sas.upenn.edu>
2016-11-20 21:57:22 -0500

    Configured static files and White Noise

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -119,11 +119,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
+)
 
 # Email settings
 
